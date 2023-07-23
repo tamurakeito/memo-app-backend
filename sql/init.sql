a0d967214b0ecf6c73c52e6ac650ffb67e180877
tamurakeito <kit_tamtam@icloud.com>
2023-07-23 12:31:51 +0900

    DB

---
+++ sql/init.sql
@@ -0,0 +1,32 @@
+USE memo_app;
+
+CREATE TABLE memo_list(
+	id INT(11) AUTO_INCREMENT NOT NULL, 
+  name VARCHAR(30) NOT NULL,
+  tag BOOLEAN NOT NULL,
+  length INT(11) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO memo_list(name, tag, length) VALUES
+  ('Todoリスト', true, 3),
+  ('買い物メモ', false, 3),
+  ('行きたい居酒屋', false, 2);
+
+CREATE TABLE task_list(
+	id INT(11) AUTO_INCREMENT NOT NULL, 
+  name VARCHAR(30) NOT NULL,
+  memo_id INT(11) NOT NULL,
+  complete BOOLEAN NOT NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO task_list(name, memo_id, complete) VALUES
+  ('タスク０', 0, false),
+  ('タスク１', 0, false),
+  ('タスク２', 0, false),
+  ('タスク３', 1, true),
+  ('タスク４', 1, false),
+  ('タスク５', 1, true),
+  ('タスク６', 2, false),
+  ('タスク７', 2, true);
