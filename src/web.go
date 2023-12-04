8ab150b64c7372bb3b6ddc1d1e4a736da275af78
tamurakeito <kit_tamtam@icloud.com>
2023-12-04 13:41:43 +0900

    cors status edit

--- src/web.go
+++ src/web.go
@@ -15,12 +15,12 @@ func main() {
 	e := echo.New()
 	// CORSの設定
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"http://localhost:3000"},
+		AllowOrigins: []string{"http://localhost:3000", "http://35.197.94.117:8080"},
 	}))
 
 	handler.InitRouting(e, memoHandler)
 	// Logger.Fatalはエラーメッセージをログに出力しアプリケーションを停止する
 	// 重要なエラーが発生した場合に使用される
 	// 普通のエラーは通常のエラーハンドリングを使おう
-	e.Logger.Fatal(e.Start(":8080"))
+	e.Logger.Fatal(e.Start(":8081"))
 }
